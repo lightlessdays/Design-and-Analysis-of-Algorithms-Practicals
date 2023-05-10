93b2c48688799d76546927f4b892a2229e20c6d8
Dhruv Badaya <dhruvbadaya@gmail.com>
2023-05-10 20:47:26 +0530

    Update 06.cpp

--- 06.cpp
+++ 06.cpp
@@ -1,79 +1,55 @@
-#include<algorithm>
-using std::swap;
-using std::partition;
-#include<string.h>
-#include<fstream>
-using std::ofstream;
-#include<iostream>
-using std::cout;
-using std::endl;
-#include<time.h>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
-int partition(int a[], int p, int r){
-    int x = a[r];
-    int i = p-1;
-    for(int j = p; j < r; j++){
-        if(a[j] <= x){
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
             i++;
-            swap(a[i], a[j]);
+            swap(arr[i], arr[j]);
         }
     }
-    swap(a[i+1], a[r]);
-    return i+1;
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
 }
 
-
-int Randomized_Partition(int a[], int p, int r){
-    int i = rand() % r + 1; //flag for debugging
-    swap(a[i], a[r]);
-    return partition(a, p, r);
+int randomizedPartition(vector<int>& arr, int low, int high) {
+    srand(time(0));
+    int random = low + rand() % (high - low + 1);
+    swap(arr[random], arr[high]);
+    return partition(arr, low, high);
 }
 
+int randomizedSelect(vector<int>& arr, int low, int high, int k) {
+    if (low == high) {
+        return arr[low];
+    }
 
-int Randomized_Select(int a[], int p, int r, int i)
-{
-    if(p==r)
-        return a[p];
-    int q = Randomized_Partition(a,p,r);
-    int k = q-p+1;
-    if(i == k)
-        return a[q];
-    else if(i < k)
-        return Randomized_Select(a, p, q-1, i);
-    else return Randomized_Select(a, q+1, r, i-k);
+    int pivotIndex = randomizedPartition(arr, low, high);
+    int length = pivotIndex - low + 1;
+
+    if (k == length) {
+        return arr[pivotIndex];
+    } else if (k < length) {
+        return randomizedSelect(arr, low, pivotIndex - 1, k);
+    } else {
+        return randomizedSelect(arr, pivotIndex + 1, high, k - length);
+    }
 }
 
-int main(){
+int main() {
+    vector<int> arr = {3, 7, 2, 9, 1, 5, 6, 8, 4};
+    int k = 5; // k-th smallest element
 
-srand(time(NULL));
-ofstream out("Project_Data.txt");
+    int result = randomizedSelect(arr, 0, arr.size() - 1, k);
+    cout << "The k-th smallest element is: " << result << endl;
 
-
-for(int n = 1000; n < 10000000; n+=100)
-{
-
-int A[n];
-for(int i = 0; i < n; i++) //intialize array of random size to random elements
-    A[i] = rand();
-
-time_t start_time = time(NULL);
-time_t current_time = time(NULL);
-double count = 0;
-
-while(current_time - start_time < 1)
-{
-    Randomized_Select(A, 0, n-1, rand()%n + 1);
-    current_time = time(NULL);
-    count ++;
+    return 0;
 }
-
-out << "Array Size: " << n << " T(n): " << 1/count
-    << " T(n)/n: " << (1/count)/n << endl;
-
-
-
-}
-
-
-}
-
