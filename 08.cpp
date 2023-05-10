5b134bbffc3a3b40445aef4f91170c4c0245bd5c
Dhruv Badaya <dhruvbadaya@gmail.com>
2023-05-10 20:50:59 +0530

    Update 08.cpp

--- 08.cpp
+++ 08.cpp
@@ -1,58 +1,60 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <stack>
+
 using namespace std;
 
-// Graph class represents a directed graph
-// using adjacency list representation
-class Graph {
-public:
-	map<int, bool> visited;
-	map<int, list<int> > adj;
+// Function to perform Depth-First Search
+void DFS(vector<vector<int>>& graph, int startNode) {
+    int numNodes = graph.size();
+    vector<bool> visited(numNodes, false); // Keep track of visited nodes
+    stack<int> stk; // Stack for DFS traversal
 
-	// function to add an edge to graph
-	void addEdge(int v, int w);
+    stk.push(startNode);
 
-	// DFS traversal of the vertices
-	// reachable from v
-	void DFS(int v);
-};
+    while (!stk.empty()) {
+        int currentNode = stk.top();
+        stk.pop();
 
-void Graph::addEdge(int v, int w)
-{
-	adj[v].push_back(w); // Add w to v’s list.
+        if (!visited[currentNode]) {
+            cout << currentNode << " ";
+            visited[currentNode] = true;
+
+            // Visit all adjacent nodes of the current node
+            for (int i = 0; i < graph[currentNode].size(); i++) {
+                int adjacentNode = graph[currentNode][i];
+                if (!visited[adjacentNode]) {
+                    stk.push(adjacentNode);
+                }
+            }
+        }
+    }
 }
 
-void Graph::DFS(int v)
-{
-	// Mark the current node as visited and
-	// print it
-	visited[v] = true;
-	cout << v << " ";
+int main() {
+    int numNodes, numEdges;
+    cout << "Enter the number of nodes: ";
+    cin >> numNodes;
 
-	// Recur for all the vertices adjacent
-	// to this vertex
-	list<int>::iterator i;
-	for (i = adj[v].begin(); i != adj[v].end(); ++i)
-		if (!visited[*i])
-			DFS(*i);
-}
+    cout << "Enter the number of edges: ";
+    cin >> numEdges;
 
-// Driver's code
-int main()
-{
-	// Create a graph given in the above diagram
-	Graph g;
-	g.addEdge(0, 1);
-	g.addEdge(0, 2);
-	g.addEdge(1, 2);
-	g.addEdge(2, 0);
-	g.addEdge(2, 3);
-	g.addEdge(3, 3);
+    vector<vector<int>> graph(numNodes);
 
-	cout << "Following is Depth First Traversal"
-			" (starting from vertex 2) \n";
+    cout << "Enter the edges in the format (u v):" << endl;
+    for (int i = 0; i < numEdges; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
 
-	// Function call
-	g.DFS(2);
+    int startNode;
+    cout << "Enter the starting node: ";
+    cin >> startNode;
 
-	return 0;
+    cout << "DFS traversal starting from node " << startNode << ": ";
+    DFS(graph, startNode);
+
+    return 0;
 }
