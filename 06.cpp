35e0813343c67414265acb6f192c9caa16ec3b3d
Dhruv Badaya <dhruvbadaya@gmail.com>
2023-05-10 20:46:12 +0530

    Update 06.cpp

--- 06.cpp
+++ 06.cpp
@@ -1,50 +1,79 @@
-#include <iostream>
-using namespace std;
+#include<algorithm>
+using std::swap;
+using std::partition;
+#include<string.h>
+#include<fstream>
+using std::ofstream;
+#include<iostream>
+using std::cout;
+using std::endl;
+#include<time.h>
 
-void lcsAlgo(char *S1, char *S2, int m, int n) {
-  int LCS_table[m + 1][n + 1];
-
-
-  // Building the mtrix in bottom-up way
-  for (int i = 0; i <= m; i++) {
-    for (int j = 0; j <= n; j++) {
-      if (i == 0 || j == 0)
-        LCS_table[i][j] = 0;
-      else if (S1[i - 1] == S2[j - 1])
-        LCS_table[i][j] = LCS_table[i - 1][j - 1] + 1;
-      else
-        LCS_table[i][j] = max(LCS_table[i - 1][j], LCS_table[i][j - 1]);
+int partition(int a[], int p, int r){
+    int x = a[r];
+    int i = p-1;
+    for(int j = p; j < r; j++){
+        if(a[j] <= x){
+            i++;
+            swap(a[i], a[j]);
+        }
     }
-  }
-
-  int index = LCS_table[m][n];
-  char lcsAlgo[index + 1];
-  lcsAlgo[index] = '\0';
-
-  int i = m, j = n;
-  while (i > 0 && j > 0) {
-    if (S1[i - 1] == S2[j - 1]) {
-      lcsAlgo[index - 1] = S1[i - 1];
-      i--;
-      j--;
-      index--;
-    }
-
-    else if (LCS_table[i - 1][j] > LCS_table[i][j - 1])
-      i--;
-    else
-      j--;
-  }
-  
-  // Printing the sub sequences
-  cout << "S1 : " << S1 << "\nS2 : " << S2 << "\nLCS: " << lcsAlgo << "\n";
+    swap(a[i+1], a[r]);
+    return i+1;
 }
 
-int main() {
-  char S1[] = "ACADB";
-  char S2[] = "CBDA";
-  int m = strlen(S1);
-  int n = strlen(S2);
 
-  lcsAlgo(S1, S2, m, n);
+int Randomized_Partition(int a[], int p, int r){
+    int i = rand() % r + 1; //flag for debugging
+    swap(a[i], a[r]);
+    return partition(a, p, r);
 }
+
+
+int Randomized_Select(int a[], int p, int r, int i)
+{
+    if(p==r)
+        return a[p];
+    int q = Randomized_Partition(a,p,r);
+    int k = q-p+1;
+    if(i == k)
+        return a[q];
+    else if(i < k)
+        return Randomized_Select(a, p, q-1, i);
+    else return Randomized_Select(a, q+1, r, i-k);
+}
+
+int main(){
+
+srand(time(NULL));
+ofstream out("Project_Data.txt");
+
+
+for(int n = 1000; n < 10000000; n+=100)
+{
+
+int A[n];
+for(int i = 0; i < n; i++) //intialize array of random size to random elements
+    A[i] = rand();
+
+time_t start_time = time(NULL);
+time_t current_time = time(NULL);
+double count = 0;
+
+while(current_time - start_time < 1)
+{
+    Randomized_Select(A, 0, n-1, rand()%n + 1);
+    current_time = time(NULL);
+    count ++;
+}
+
+out << "Array Size: " << n << " T(n): " << 1/count
+    << " T(n)/n: " << (1/count)/n << endl;
+
+
+
+}
+
+
+}
+
