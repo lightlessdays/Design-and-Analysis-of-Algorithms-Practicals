eb6c581c7c342f59e866697b7e9955ee331c849c
Dhruv Badaya <dhruvbadaya@gmail.com>
2023-05-10 17:20:09 +0530

    Update 04.cpp

--- 04.cpp
+++ 04.cpp
@@ -1,73 +1,68 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 
-// A utility function to get maximum value in arr[]
-int getMax(int arr[], int n)
-{
-	int mx = arr[0];
-	for (int i = 1; i < n; i++)
-		if (arr[i] > mx)
-			mx = arr[i];
-	return mx;
+// Function to find the maximum element in the array
+int findMax(std::vector<int>& arr, int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
 }
 
-// A function to do counting sort of arr[] according to
-// the digit represented by exp.
-void countSort(int arr[], int n, int exp)
-{
-	int output[n]; // output array
-	int i, count[10] = { 0 };
+// Using counting sort to sort the elements based on the place value
+void countingSort(std::vector<int>& arr, int n, int exp) {
+    std::vector<int> output(n);
+    std::vector<int> count(10, 0);
 
-	// Store count of occurrences in count[]
-	for (i = 0; i < n; i++)
-		count[(arr[i] / exp) % 10]++;
+    // Store count of occurrences in count[]
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
 
-	// Change count[i] so that count[i] now contains actual
-	// position of this digit in output[]
-	for (i = 1; i < 10; i++)
-		count[i] += count[i - 1];
+    // Change count[i] so that count[i] contains the actual
+    // position of this digit in output[]
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
 
-	// Build the output array
-	for (i = n - 1; i >= 0; i--) {
-		output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-		count[(arr[i] / exp) % 10]--;
-	}
+    // Build the output array
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
 
-	// Copy the output array to arr[], so that arr[] now
-	// contains sorted numbers according to current digit
-	for (i = 0; i < n; i++)
-		arr[i] = output[i];
+    // Copy the output array to arr[] so that arr[] contains
+    // sorted numbers according to the current digit
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
 }
 
-// The main function to that sorts arr[] of size n using
-// Radix Sort
-void radixsort(int arr[], int n)
-{
-	// Find the maximum number to know number of digits
-	int m = getMax(arr, n);
+// Radix Sort function
+void radixSort(std::vector<int>& arr, int n) {
+    // Find the maximum number to determine the number of digits
+    int max = findMax(arr, n);
 
-	// Do counting sort for every digit. Note that instead
-	// of passing digit number, exp is passed. exp is 10^i
-	// where i is current digit number
-	for (int exp = 1; m / exp > 0; exp *= 10)
-		countSort(arr, n, exp);
+    // Perform counting sort for every digit
+    for (int exp = 1; max / exp > 0; exp *= 10)
+        countingSort(arr, n, exp);
 }
 
-// A utility function to print an array
-void print(int arr[], int n)
-{
-	for (int i = 0; i < n; i++)
-		cout << arr[i] << " ";
-}
+int main() {
+    int numValues;
+    std::cout << "Enter the number of values: ";
+    std::cin >> numValues;
 
-// Driver Code
-int main()
-{
-	int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	
-	// Function Call
-	radixsort(arr, n);
-	print(arr, n);
-	return 0;
+    std::vector<int> arr(numValues);
+    std::cout << "Enter the values:\n";
+    for (int i = 0; i < numValues; i++)
+        std::cin >> arr[i];
+
+    radixSort(arr, numValues);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < numValues; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+    return 0;
 }
